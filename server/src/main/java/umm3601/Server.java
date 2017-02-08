936a63fcc8bcddbdd28a9c696cec566ddf3401b5
devshawn <musicalshawn@gmail.com>
2017-02-08 15:33:49 -0600

    Adding to do list API jar and server endpoints

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import umm3601.todo.ToDoController;
 import umm3601.user.UserController;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class Server {
 
         Gson gson = new Gson();
         UserController userController = new UserController();
+        ToDoController toDoController = new ToDoController();
 
         options("/*", (request, response) -> {
 
@@ -50,6 +52,19 @@ public class Server {
             res.type("application/json");
             String id = req.params("id");
             return gson.toJson(userController.getUser(id));
+        });
+
+        // List todos
+        get("api/todos", (req, res) -> {
+            res.type("application/json");
+            return gson.toJson(toDoController.listToDos(req.queryMap().toMap()));
+        });
+
+        // See specific todo
+        get("api/todos/:id", (req, res) -> {
+            res.type("application/json");
+            String id = req.params("id");
+            return gson.toJson(toDoController.getToDo(id));
         });
 
         // Handle "404" file not found requests:
