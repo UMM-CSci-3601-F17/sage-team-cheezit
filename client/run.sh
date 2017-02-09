331b9ede8fdb1c652464b09bda5f0b638b10232b
devshawn <musicalshawn@gmail.com>
2017-02-09 13:46:52 -0600

    Redoing runClient, fixes #13

--- client/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-exec ./yarn/yarn-latest/bin/yarn run start &
-
-echo $! > yarn-server-pid.lock
--- client/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pkill -TERM -P  $(cat yarn-server-pid.lock) && rm yarn-server-pid.lock
