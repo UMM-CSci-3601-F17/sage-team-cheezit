7453fca0196db941888430a75ca6036ee37bff1b
Nic McPhee <mcphee@morris.umn.edu>
2017-02-14 13:32:54 -0600

    Fix URL bug in `UserListService`
    I had tried to clean up the URL handling in `UserListService`, but didn't
    test it properly and we rolled it out in a broken state.
    
    We probably want to add some tests for the service to catch problems
    like this in the future.
    
    Fixes #24

--- client/src/app/users/user-list.service.ts
+++ client/src/app/users/user-list.service.ts
@@ -5,14 +5,14 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class UserListService {
-    private userUrl: string = API_URL + "users/";
+    private userUrl: string = API_URL + "users";
     constructor(private http:Http) { }
 
     getUsers(): Observable<User[]> {
-        return this.http.request(this.userUrl + 'users').map(res => res.json());
+        return this.http.request(this.userUrl).map(res => res.json());
     }
 
     getUserById(id: string): Observable<User> {
-        return this.http.request(this.userUrl + id).map(res => res.json());
+        return this.http.request(this.userUrl + "/" + id).map(res => res.json());
     }
 }
