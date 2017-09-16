9672df89ef33fa22885bc7b6c0f40b1abeb64199
frazi177 <frazi177@morris.umn.edu>
2017-09-16 14:37:11 -0500

    Bring template code for Angular 4
    Also moves old sources to srcS17

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,24 +1,33 @@
-module.exports = function (config) {
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine'],
-        files: [
-            { pattern: 'src/test.js', watched: false }
-        ],
-        exclude: [
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-        ],
-        preprocessors: {
-            'src/test.js': ['webpack', 'sourcemap']
-        },
-        webpack: require('./webpack.test.js'),
-        reporters: ['progress'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: false,
-        browsers: ['PhantomJS'],
-        singleRun: true,
-        concurrency: Infinity
-    })
-}
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma')
+    ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app',
-    templateUrl: 'app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-// App Component class
-export class AppComponent {}
+export class AppComponent {
+  title = 'app';
+}
