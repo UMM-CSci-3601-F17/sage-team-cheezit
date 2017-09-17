ee402aba4f124e24fe614b8cab692ac16375bbfc
Nic McPhee <mcphee@morris.umn.edu>
2017-09-17 11:14:17 -0500

    Get default e2e tests to pass
    This changes the text that is looked for on the "main" page
    so that the default e2e tests pass.
    
    This starts work on issue #27

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('angular-spark-lab App', () => {
+describe('angular-spark-lab', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should have a "title"', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Angular Spark lab');
   });
 });
