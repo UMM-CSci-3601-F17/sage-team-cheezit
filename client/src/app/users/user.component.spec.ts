5a184a64b2e294dff63d9408cb629be878611efd
= <dansaoficial@gmail.com>
2017-09-26 12:53:44 -0500

    Change id to _id

--- client/src/app/users/user.component.spec.ts
+++ client/src/app/users/user.component.spec.ts
@@ -19,27 +19,27 @@ describe("User component", () => {
         userListServiceStub = {
             getUserById: (userId: string) => Observable.of([
                 {
-                    id: "chris_id",
+                    _id: "chris_id",
                     name: "Chris",
                     age: 25,
                     company: "UMM",
                     email: "chris@this.that"
                 },
                 {
-                    id: "pat_id",
+                    _id: "pat_id",
                     name: "Pat",
                     age: 37,
                     company: "IBM",
                     email: "pat@something.com"
                 },
                 {
-                    id: "jamie_id",
+                    _id: "jamie_id",
                     name: "Jamie",
                     age: 37,
                     company: "Frogs, Inc.",
                     email: "jamie@frogs.com"
                 }
-            ].find(user => user.id === userId))
+            ].find(user => user._id === userId))
         };
 
         TestBed.configureTestingModule({
--- client/src/app/users/user.ts
+++ client/src/app/users/user.ts
@@ -1,5 +1,5 @@
 export interface User {
-    id: string,
+    _id: string,
     name: string,
     age: number,
     company: string,
