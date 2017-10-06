7585e7fd0f548e4a9552be2d34b70ccf18d69ca3
Paul Friederichsen <floogulinc@gmail.com>
2017-10-06 02:34:31 -0500

    Replace Bootstrap with Covalent and Angular Material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CovalentLayoutModule, CovalentStepsModule, CovalentCommonModule /*, any other modules */ } from '@covalent/core';
+
+import {
+    MATERIAL_COMPATIBILITY_MODE, MatListModule, MdButtonModule, MdCardModule, MdIconModule, MdInputModule, MdMenuModule,
+    MdSidenavModule,
+    MdToolbarModule
+} from '@angular/material';
+
+import { FlexLayoutModule, } from '@angular/flex-layout';
+
 import {HttpModule, JsonpModule} from '@angular/http';
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
 import {HomeComponent} from './home/home.component';
 import {UserComponent} from "./users/user.component";
 import {UserListComponent} from './users/user-list.component';
@@ -15,21 +25,34 @@ import {APP_BASE_HREF} from "@angular/common";
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpModule,
         JsonpModule,
         Routing,
         FormsModule,
+        CovalentLayoutModule,
+        CovalentStepsModule,
+        CovalentCommonModule,
+        FlexLayoutModule,
+        MatListModule,
+        MdButtonModule,
+        MdIconModule,
+        MdToolbarModule,
+        MdCardModule,
+        MdMenuModule,
+        MdSidenavModule,
+        MdInputModule
     ],
     declarations: [
         AppComponent,
         HomeComponent,
-        NavbarComponent,
         UserListComponent,
         UserComponent
     ],
     providers: [
         UserListService,
-        {provide: APP_BASE_HREF, useValue: '/'}
+        {provide: APP_BASE_HREF, useValue: '/'},
+        {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}
     ],
     bootstrap: [AppComponent]
 })
