f531e9b9681143b0e31d1b4fba23c7db022d3eff
Paul Friederichsen <floogulinc@gmail.com>
2017-10-09 05:22:15 -0500

    Add a basic card component to the frontend
    This component is used to display a card that is passed into it. It still needs more styling.

---
+++ client/src/app/card-component/card.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Card} from "../card/card";
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() card: Card;
+
+  ngOnInit() {
+  }
+
+}
