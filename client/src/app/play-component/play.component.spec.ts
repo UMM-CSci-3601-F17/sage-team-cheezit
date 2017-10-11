7ffc39ba13bb9e9339333cf63845698371e47e6b
Paul Friederichsen <floogulinc@gmail.com>
2017-10-11 18:18:05 -0500

    Fix the basic testing
    Now things pass, we still need to add all the tests but the structure is there.

--- client/src/app/play-component/play.component.spec.ts
+++ client/src/app/play-component/play.component.spec.ts
@@ -1,14 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayComponent } from './play.component';
+import {Observable} from "rxjs/Observable";
+import {Deck} from "../deck/deck";
+import {MATERIAL_COMPATIBILITY_MODE} from "@angular/material";
+import {DeckService} from "../deck/deck.service";
+import {ActivatedRoute} from "@angular/router";
+import {SharedModule} from "../shared.module";
+import {CardComponent} from "../card-component/card.component";
 
 describe('PlayComponent', () => {
   let component: PlayComponent;
   let fixture: ComponentFixture<PlayComponent>;
 
+    let deckServiceStub: {
+        getDeck: (id) => Observable<Deck>
+    };
+
   beforeEach(async(() => {
+
+      deckServiceStub = {
+          getDeck: (id) => Observable.of({
+              _id : {
+                  $oid: "test id"
+              },
+              name: "test deck",
+              cards: [
+                  {   _id : "test id",
+                      word : "test word",
+                      synonym : "test synonym",
+                      antonym: "test antonym",
+                      general_sense: "test general_sense",
+                      example_usage: "test example_usage",
+                  }
+              ]
+          })
+      };
+
     TestBed.configureTestingModule({
-      declarations: [ PlayComponent ]
+        imports: [SharedModule],
+      declarations: [ PlayComponent, CardComponent ],
+        providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
+            {provide: DeckService, useValue: deckServiceStub}, {
+                provide: ActivatedRoute,
+                useValue: {
+                    params: Observable.of({id: "test id"})
+                }
+            }],
     })
     .compileComponents();
   }));
