badc87efeb9902bbaa1a15f46bdcd7332ecd38c5
Paul Friederichsen <floogulinc@gmail.com>
2017-10-12 16:50:37 -0500

    Remove unused CardState in card component

--- client/src/app/card-component/card.component.ts
+++ client/src/app/card-component/card.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Card} from "../card/card";
-import {CardState} from "../play-component/CardState";
 
 @Component({
   selector: 'app-card',
@@ -8,10 +7,8 @@ import {CardState} from "../play-component/CardState";
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-    cardState: CardState;
 
   constructor() {
-    this.cardState = new CardState;
   }
 
   @Input() card: Card;
