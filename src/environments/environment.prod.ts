e5b884c9fc64e1b3ed569c5428cea9203ed395d7
Paul Friederichsen <floogulinc@gmail.com>
2017-10-25 19:51:14 -0500

    Add temporary development Firebase API info

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,11 @@ export const environment = {
     production: true,
 
     firebase: {
+        apiKey: "AIzaSyCii_NTRaKjlbofrh0xI_SbBpOAE9vj7wg",
+        authDomain: "sage-2cfe4.firebaseapp.com",
+        databaseURL: "https://sage-2cfe4.firebaseio.com",
+        projectId: "sage-2cfe4",
+        storageBucket: "sage-2cfe4.appspot.com",
+        messagingSenderId: "999732247851"
     }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,11 @@ export const environment = {
     production: false,
 
     firebase: {
+        apiKey: "AIzaSyCii_NTRaKjlbofrh0xI_SbBpOAE9vj7wg",
+        authDomain: "sage-2cfe4.firebaseapp.com",
+        databaseURL: "https://sage-2cfe4.firebaseio.com",
+        projectId: "sage-2cfe4",
+        storageBucket: "sage-2cfe4.appspot.com",
+        messagingSenderId: "999732247851"
     }
 };
