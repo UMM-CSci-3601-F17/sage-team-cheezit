95095e502a47cdc1eaabe3ad115aba7d81c42278
Paul Friederichsen <floogulinc@gmail.com>
2017-10-31 23:43:54 -0500

    Add NgPipesModule to shared modules

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -28,6 +28,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {PageSliderModule} from "ng2-page-slider";
 import {AngularFireAuthModule} from "angularfire2/auth";
 import {AngularFirestoreModule} from "angularfire2/firestore";
+import {NgPipesModule} from "ngx-pipes";
 
 const FLEX_LAYOUT_MODULES: any[] = [
     FlexLayoutModule,
@@ -64,6 +65,7 @@ const OTHER_MODULES: any[] = [
     PageSliderModule,
     AngularFirestoreModule,
     AngularFireAuthModule,
+    NgPipesModule,
 ];
 
 
