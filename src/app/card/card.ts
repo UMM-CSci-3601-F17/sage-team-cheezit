44c8b547919c35d2f0dcc716c7a8efb333967172
Paul Friederichsen <floogulinc@gmail.com>
2017-11-28 21:59:02 -0600

    Add multiple hints to Card interface
    Also creates a new PlayCard interface to be used in play views

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,10 +1,18 @@
 export interface Card {
     //_id: string,
     word: string,
+    synonym: string[],
+    antonym: string[],
+    general_sense: string[],
+    example_usage: string[],
+    hidden: boolean
+}
+export interface PlayCard {
+    word: string,
     synonym: string,
     antonym: string,
     general_sense: string,
     example_usage: string,
-    hidden: boolean
 }
+
 export interface CardId extends Card { id: string }
