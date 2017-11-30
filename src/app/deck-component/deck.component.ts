bff735b08cd8bc00b93184c6b564cb96fa832aac
M1NN <score@morris.umn.edu>
2017-11-29 19:18:21 -0600

    Sort cards in deck view alphabetically

--- src/app/deck-component/deck.component.ts
+++ src/app/deck-component/deck.component.ts
@@ -80,7 +80,7 @@ export class DeckComponent implements OnInit, OnDestroy {
                 }
             );
 
-            this.deckService.getDeckCards(this.id).takeUntil(componentDestroyed(this)).subscribe(cards => {
+            this.deckService.getDeckCards(this.id, ref => ref.orderBy('word')).takeUntil(componentDestroyed(this)).subscribe(cards => {
                 console.log(cards);
                 this.cards = cards;
             });
