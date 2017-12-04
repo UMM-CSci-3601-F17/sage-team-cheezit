404f6e50680b730489c362f5ec54a5d0dc966e8f
Paul Friederichsen <floogulinc@gmail.com>
2017-12-03 19:56:03 -0600

    Fix testing error with creation of EditableTextComponent

--- src/app/editable-text/editable-text.component.spec.ts
+++ src/app/editable-text/editable-text.component.spec.ts
@@ -1,25 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { EditableTextComponent } from './editable-text.component';
+import {EditableTextComponent} from './editable-text.component';
+import {SharedModule} from "../shared.module";
 
 describe('EditableTextComponent', () => {
-  let component: EditableTextComponent;
-  let fixture: ComponentFixture<EditableTextComponent>;
+    let component: EditableTextComponent;
+    let fixture: ComponentFixture<EditableTextComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ EditableTextComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [SharedModule],
+            declarations: [EditableTextComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditableTextComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(EditableTextComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
