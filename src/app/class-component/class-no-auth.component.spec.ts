f14f707845401d494b0b3a453c861bc6642536a9
bushw011 <bushw011@morris.umn.edu>
2017-12-04 21:27:20 -0600

    Add a class spec test file with no authorization, and tests for correct edit permission

---
+++ src/app/class-component/class-no-auth.component.spec.ts
@@ -0,0 +1,63 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ClassComponent} from './class.component';
+import {Class} from "../class/class";
+import {SharedModule} from "../shared.module";
+import {MATERIAL_COMPATIBILITY_MODE} from "@angular/material";
+import {ClassServiceMock} from "../class/class.service.mock";
+import {DeckServiceMock} from "../deck/deck.service.mock";
+import {DeckService} from "../deck/deck.service";
+import {ClassService} from "../class/class.service";
+import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs/Observable";
+import {AppTestModule} from "../app.test.module";
+import {AngularFireAuth} from "angularfire2/auth";
+
+describe('ClassComponent', () => {
+
+    let component: ClassComponent;
+    let fixture: ComponentFixture<ClassComponent>;
+
+    let mockFirebaseAuth = {
+        auth: {
+            currentUser: {
+                uid: "BadTestUid"
+            }
+        }
+    };
+
+    beforeEach(async(() => {
+
+        TestBed.configureTestingModule({
+            imports: [SharedModule, AppTestModule],
+            declarations: [],
+            providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
+                {provide: DeckService, useValue: new DeckServiceMock()},
+                {provide: ClassService, useValue: new ClassServiceMock()},
+                {provide: AngularFireAuth, useValue: mockFirebaseAuth},
+                { provide: ActivatedRoute,
+                    useValue: {
+                        params: Observable.of({id: "test id"})
+                    } }],
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ClassComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+
+
+    it('should not have edit permissions, because authorization has not been passed in', () => {
+        expect(component.canEdit).toEqual(false);
+    });
+
+
+});
