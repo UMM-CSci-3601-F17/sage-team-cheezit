bf5ab6a8fa20e1f1e16946bc64f52df2283a3a69
Nick Plucker <pluck011@morris.umn.edu>
2017-12-05 20:22:31 -0600

    Fix deck service mock and add more testing for my decks

--- src/app/my-decks/my-decks.component.spec.ts
+++ src/app/my-decks/my-decks.component.spec.ts
@@ -5,20 +5,42 @@ import {SharedModule} from "../shared.module";
 import {AppTestModule} from "../app.test.module";
 import {By} from "@angular/platform-browser";
 import {DebugElement} from "@angular/core";
+import {AngularFireAuth} from "angularfire2/auth";
+import {MATERIAL_COMPATIBILITY_MODE} from "@angular/material";
+import {Observable} from "rxjs/Observable";
+import {ActivatedRoute} from "@angular/router";
+import {DeckService} from "../deck/deck.service";
+import {DeckServiceMock} from "../deck/deck.service.mock";
 
 describe('MyDecksComponent', () => {
     let component: MyDecksComponent;
     let fixture: ComponentFixture<MyDecksComponent>;
     let debugElement: DebugElement;
 
+    let mockFirebaseAuth = {
+        auth: {
+            currentUser: {
+                uid: "testuid"
+            }
+        }
+    };
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [SharedModule, AppTestModule],
-            declarations: []
+            declarations: [],
+            providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
+                {provide: DeckService, useValue: new DeckServiceMock()},
+                {provide: AngularFireAuth, useValue: mockFirebaseAuth},
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: Observable.of({id: "test id"})
+                    }
+                }],
         })
             .compileComponents();
     }));
-
     beforeEach(() => {
         fixture = TestBed.createComponent(MyDecksComponent);
         component = fixture.componentInstance;
@@ -31,7 +53,17 @@ describe('MyDecksComponent', () => {
     });
 
     it('should contain title My Decks', () => {
-       let title: HTMLElement = debugElement.query(By.css('.decks-title')).nativeElement;
-       expect(title.innerText).toContain('My Decks');
+        let title: HTMLElement = debugElement.query(By.css('.decks-title')).nativeElement;
+        expect(title.innerText).toContain('My Decks');
+    });
+
+    it('should contain a button called Add Deck', () => {
+        let button: HTMLElement = debugElement.query(By.css('.md-24')).nativeElement;
+        expect(button.innerText).toContain('add');
+    });
+
+    it('should contain a button for Add Deck', () => {
+        let button: HTMLElement = debugElement.query(By.css('.md-24')).nativeElement;
+        expect(button).toBeTruthy();
     });
 });
