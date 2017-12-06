7e0e2e8f201029e339d3d714c13bac187e894b87
M1NN <score@morris.umn.edu>
2017-12-05 20:20:26 -0600

    Add tests for join-game, play-view, and one for public-decks

--- src/app/join-game/join-game.component.spec.ts
+++ src/app/join-game/join-game.component.spec.ts
@@ -2,16 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JoinGameComponent } from './join-game.component';
 import {SharedModule} from "../shared.module";
+import {MATERIAL_COMPATIBILITY_MODE} from "@angular/material";
 import {AppTestModule} from "../app.test.module";
+import {DebugElement} from '@angular/core';
+import {By} from "@angular/platform-browser";
 
 describe('JoinGameComponent', () => {
   let component: JoinGameComponent;
   let fixture: ComponentFixture<JoinGameComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
         imports: [SharedModule, AppTestModule],
-      declarations: [ ]
+        declarations: [],
+        providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
     })
     .compileComponents();
   }));
@@ -19,10 +24,37 @@ describe('JoinGameComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(JoinGameComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a title with the text stating Join Game', () => {
+      let title: HTMLElement = debugElement.query(By.css('.title')).nativeElement;
+      expect(title.innerText).toEqual('Join Game');
+  });
+
+  it('should contain a join button with the text stating Join Game', () => {
+      let joinButton: HTMLElement = debugElement.query(By.css('.join-button')).nativeElement;
+      expect(joinButton.innerText).toEqual('Join Game');
+  });
+
+  it('should do nothing after clicking Join Game since no join code has been entered', () => {
+      let joinButton: HTMLElement = debugElement.query(By.css('.join-button')).nativeElement;
+      joinButton.click();
+      let title: HTMLElement = debugElement.query(By.css('.title')).nativeElement;
+      // Join Game title would not still be there if the join code was correct
+      expect(title.innerText).toEqual('Join Game');
+  });
+  it('should do nothing after clicking Join Game since a wrong join code has been entered', () => {
+      component.gameId = "wrong";
+      let joinButton: HTMLElement = debugElement.query(By.css('.join-button')).nativeElement;
+      joinButton.click();
+      let title: HTMLElement = debugElement.query(By.css('.title')).nativeElement;
+      // Join Game title would not still be there if the join code was correct
+      expect(title.innerText).toEqual('Join Game');
   });
 });
--- src/app/public-decks/public-decks.component.spec.ts
+++ src/app/public-decks/public-decks.component.spec.ts
@@ -3,15 +3,20 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {PublicDecksComponent} from './public-decks.component';
 import {AppTestModule} from "../app.test.module";
 import {SharedModule} from "../shared.module";
+import {MATERIAL_COMPATIBILITY_MODE} from "@angular/material";
+import {DebugElement} from '@angular/core';
+import {By} from "@angular/platform-browser";
 
 describe('PublicDecksComponent', () => {
     let component: PublicDecksComponent;
     let fixture: ComponentFixture<PublicDecksComponent>;
+    let debugElement: DebugElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [SharedModule, AppTestModule],
-            declarations: []
+            declarations: [],
+            providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
         })
             .compileComponents();
     }));
@@ -19,10 +24,16 @@ describe('PublicDecksComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(PublicDecksComponent);
         component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should have a title that says Public Decks', () => {
+        let title: HTMLElement = debugElement.query(By.css('#decks-title')).nativeElement;
+        expect(title.innerText).toEqual('Public Decks');
     });
 });
